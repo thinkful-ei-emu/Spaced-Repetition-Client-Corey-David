8e644d7cfd743c7760d69125bc54ce3f9403280d
Corey Moore <monorthwest@gmail.com>
2019-09-09 07:34:49 -0700

    init

---
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class DashboardRoute extends Component {
+  render() {
+    return (
+      <section>
+        implement and style me : dashboardRoute
+      </section>
+    );
+  }
+}
+
+export default DashboardRoute
