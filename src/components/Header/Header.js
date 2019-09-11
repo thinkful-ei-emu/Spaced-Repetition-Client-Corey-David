9aebecc37498d295af42395cc58939b0ab1a8d2e
Corey Moore <monorthwest@gmail.com>
2019-09-11 13:30:39 -0700

    learning route

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -14,7 +14,7 @@ class Header extends Component {
   renderLogoutLink() {
     return (
       <div>
-        <span>
+        <span id='user-name'>
           {this.context.user.name}
         </span>
         <nav>
--- src/routes/LearningRoute/LearningRoute.js
+++ src/routes/LearningRoute/LearningRoute.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import Api from '../../services/Api.service';
+import Button from '../../components/Button/Button';
+import './Learning.css'
 
 class LearningRoute extends Component {
 
@@ -25,9 +27,9 @@ class LearningRoute extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-    console.log('test');
+    console.log('test for learning submit');
     let input = e.target['answer-input'].value
-    // Api.doFetch('language/guess', 'POST', input)
+    Api.doFetch('language/guess', 'POST', input)
       
     e.target['answer-input'].value = '';
   }
@@ -35,21 +37,24 @@ class LearningRoute extends Component {
 
   render() {
     return (
-      <section>
+      <section className='learning-container'>
         
         <h2>Translate the word:</h2>
+        <div className='next-word-container'>
         <span id='next-word'>{this.state.nextWord}</span>
+        </div>
         <p>Your total score is: {this.state.totalScore}</p>
         <p>You have answered this word correctly {this.state.wordCorrectCount} times.</p>
         <p>You have answered this word incorrectly {this.state.wordIncorrectCount} times.</p>
         <form onSubmit={(e)=>this.handleSubmit(e)}>
           <label htmlFor='learn-guess-input'>What's the translation for this word?</label>
           <input name='answer-input' id='learn-guess-input' type='text' required></input>
-          <button type='submit'>Submit your answer</button>
+          {/* <button type='submit'>Submit your answer</button> */}
+          <Button type='submit'>Submit your answer</Button>
         </form>
       </section>
     );
   }
 }
 
-export default LearningRoute
+export default LearningRoute;
