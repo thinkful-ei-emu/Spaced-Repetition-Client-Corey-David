a562b5c028ee6529cfb6aeb02b0557ac9d2eb1e0
David Queen <dsqjr@msn.com>
2019-10-11 13:40:36 -0400

    changed api to point to remote server

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT:/* 'https://protected-ravine-87000.herokuapp.com/api' */'http://localhost:8000/api',
+  API_ENDPOINT:'https://protected-ravine-87000.herokuapp.com/api',
   TOKEN_KEY: 'blogful-client-auth-token',
 }
